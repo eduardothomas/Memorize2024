6cccf7f1e0084459b4589a422b623a58697fa2c4
Eduardo Thomas <eduardothomas64@gmail.com>
2024-02-14 14:38:39 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Eduardo Thomas on 02/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
